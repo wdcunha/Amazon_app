00df14b0992ce44431b4bb63aa7e41bb70fc4a00
Wellington <wdcunha@gmail.com>
2017-12-07 14:57:56 -0800

    Beginning Amazon_App project with welcome created and running

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get('/', { to: 'welcome#index', as: :home })
+  # get "/hello/:name" => "welcome#hello"
+  # get "/hello/:name" => "welcome#hello",
+
+end
